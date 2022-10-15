87d12ea21c5f59feabdc3684da89cf84e28d304e
Benjamin Wilson-Langman <benjamin-wilson-langman@uiowa.edu>
2022-10-15 15:59:26 -0500

    Moved JS code to relevant file

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,13 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
+var heatmapInstance = h337.create({
+    container: document.getElementById('heatMap')
+  });
+
+  var testData = {
+        min: 0,
+        max: 100,
+        data: [{x: 807, y: 583, value: 65}, {x: 597, y: 285, value: 51}, {x: 217, y: 449, value: 73}, {x: 377, y: 656, value: 58}, {x: 467, y: 509, value: 47}, {x: 487, y: 164, value: 46}, {x: 247, y: 194, value: 35}]
+  };
+  heatmapInstance.setData(testData);

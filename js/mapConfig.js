f99131480ffb3b79d0bb407c13d47c2941ea0842
Benjamin Wilson-Langman <benjamin-wilson-langman@uiowa.edu>
2022-10-15 20:18:32 -0500

    Updated starting position

--- js/mapConfig.js
+++ js/mapConfig.js
@@ -2,9 +2,10 @@ let map;
 
   function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
-      center: { lat: -34.397, lng: 150.644 },
+      center: { lat: 41.6611, lng: -91.5302 },
       zoom: 8,
     });
   }
 
   window.initMap = initMap;
+
